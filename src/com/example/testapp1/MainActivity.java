dba6283811a3bed2fa7a2b77ac225d6c871f5d26
DTHeaven <980666540@qq.com>
2013-12-22 19:50:40 +0800

    33

--- src/com/example/testapp1/MainActivity.java
+++ src/com/example/testapp1/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.Window;
 
 public class MainActivity extends SlidingFragmentActivity {
 
-	private static final String TAG = "7456MainActivity";
+	private static final String TAG = "8456MainActivity";
 	
 	protected SlidingMenu mSlidingMenu;
 	protected ListFragment mFrag;
